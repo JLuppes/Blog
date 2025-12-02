a5ebbd4a507fc8b77df073bf645b965e9b115391
Joshua Luppes <lupjos@dunwoody.edu>
2025-12-02 11:35:19 -0600

    Add user list

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,10 +1,19 @@
-from flask import request, Response, json, Blueprint
+from flask import request, Response, json, Blueprint, render_template
+from src.models.models import Post, Assignment, Categorization, Comment, Profile, Role, Tag, User
 
 # user controller blueprint to be registered with api blueprint
 users = Blueprint("users", __name__)
 
+
+@users.route('/')
+def users_list():
+    user_list = User.query.all()
+
+    return render_template('users/userlist.html.jinja', users=user_list)
+
+
 # route for login api/users/signin
-@users.route('/signin', methods = ["GET", "POST"])
+@users.route('/signin', methods=["GET", "POST"])
 def handle_login():
     return Response(
         response=json.dumps({'status': "success"}),
@@ -13,7 +22,9 @@ def handle_login():
     )
 
 # route for login api/users/signup
-@users.route('/signup', methods = ["GET", "POST"])
+
+
+@users.route('/signup', methods=["GET", "POST"])
 def handle_signup():
     return Response(
         response=json.dumps({'status': "success"}),
