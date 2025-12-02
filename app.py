4e1245af74c0bda769fda4e10bd8d3b04d094e59
Joshua Luppes <lupjos@dunwoody.edu>
2025-12-02 12:21:11 -0600

    Add demo user and role

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from src import config, app, db
-
-
+from flask_security import hash_password
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
+        
+    # one time setup
+    with app.app_context():
+        # init_db()
+        # Create a user and role to test with
+        app.security.datastore.find_or_create_role(
+            name="user", permissions={"user-read", "user-write"}, title="user"
+        )
+        db.session.commit()
+        if not app.security.datastore.find_user(email="test@me.com"):
+            app.security.datastore.create_user(email="test@me.com",
+            password=hash_password("password"), roles=["user"])
+        db.session.commit()
 
     app.run(host=config.HOST,
             port=config.PORT,
