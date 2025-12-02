7f73cff400f00d46a516a81ca15d5ec3d5d38db1
Joshua Luppes <lupjos@dunwoody.edu>
2025-12-02 10:40:47 -0600

    Move api routes to main app

--- src/__init__.py
+++ src/__init__.py
@@ -207,6 +207,46 @@ app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'morph'
 bootstrap = Bootstrap5(app)
 
 
+@api.route("/health", methods=["GET"])
+@auth_required("session")
+def health():
+    return jsonify(secret="lush oranges", date=datetime.now(timezone.utc))
+
+
+@api.route("/popmail", methods=["GET"])
+def popmail():
+    # This gets and pops the most recently sent email
+    # Please please do not do this in your real application!
+    mailer = current_app.extensions["mail"]
+    sent = mailer.pop()
+    if sent:
+        return jsonify(mail=sent)
+    abort(400)
+
+
+@api.route("/popsms", methods=["GET"])
+def popsms():
+    # This gets and pops the most recently sent SMS
+    # Please please do not do this in your real application!
+    msg = SmsCaptureSender.pop()
+    if msg:
+        return jsonify(sms=msg)
+    abort(400)
+
+
+@api.route("/resetfresh", methods=["GET"])
+def resetfresh():
+    # This resets the callers session freshness field - just for testing
+    old_paa = (
+        session["fs_paa"]
+        - current_app.config["SECURITY_FRESHNESS"].total_seconds()
+        - 100
+    )
+    session["fs_paa"] = old_paa
+    session.pop("fs_gexp", None)
+    return jsonify()
+
+
 app.register_blueprint(api, url_prefix="/api")
 app.register_blueprint(mainNav, url_prefix="/")
 
